4c7de15699abea24e832c411c66e16fa693c850a
Igor <nitigor100@gmail.com>
2019-10-01 19:15:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import axios from 'axios';
+import Header from "./components/Header";
+import Loading from "./components/Loading";
+//import { marvelApi as config } from '../config'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);  
+    this.state = {
+        posts: []        
+      };  
+  }
+
+
+  componentDidMount() {
+       
+    axios.get( "https://api.nasa.gov/planetary/apod?api_key=SkaeQVc2fa3Ffle5ixCgzhvuCLeNwRvjRU6RLlzI")
+      .then(res =>{
+        const posts = res.data;
+        console.log(res.data);
+        this.setState({posts});        
+      })
+
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+
+
+  //   axios.get( "https://rxhormone.com/wp-json/wp/v2/posts")
+  //     .then(res =>{
+  //       const posts = res.data;
+  //       this.setState({posts});        
+  //   });
+  // } 
+  
+ 
+/*
+  async componentDidMount() {
+    // error handling is important when fetching data
+    try {
+      await fetch(this.state.dataRoute)
+        .then(res => res.json())
+        .then(sections =>
+          this.setState((prevState, props) => {
+            //return { sections: sections};
+            return { sections: sections.map(this.mapSection) };
+          })
+        );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  */
+
+  /*
+
+  mapSection(section) {
+    return {
+      title: section.title,
+      content: section.content.rendered      
+    };
+  }
+  */
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+        <div className="main">
+          {this.state.posts.length === 0 && <Loading />}
+          {console.log(this.state.posts)}
+          <ul>
+      
+          </ul>
+        </div>    
+        <footer>
+          <p>Simple page</p>
+        </footer>       
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+//import logo from './../logo.png';
+
+class Header extends Component {
+  render() {   
+    return (
+      <div>
+        <div>         
+        </div>
+        <h1 className="App-title">React Marvel SPA</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
